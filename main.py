8c2594ea6d092a2133b7cc92170d819b5a698769
Heiholf <heiholf@gmx.de>
2021-04-16 14:07:42 +0200

    Simple message detection system

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
-
 import discord
+
 
 class Client(discord.Client):
     async def on_ready(self):
         print("Bot initialized...")
+
+    async def on_message(self, message):
+        if message.author == client.user:
+            return
+        await message.channel.send("Message received.")
+
 
 client = Client()
 client.run("ODMyMjE3MzIyOTgyNDczNzk4.YHgkxw.f2TwACwDW2qxSOuzJtXrOwnuGYI")
