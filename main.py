5182bbd15c660b5711c659104401e751b59e78fa
Heiholf <heiholf@gmx.de>
2021-04-16 14:02:38 +0200

    Bot setup

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("Hello wo")
+
+import discord
+
+class Client(discord.Client):
+    async def on_ready(self):
+        print("Bot initialized...")
+
+client = Client()
+client.run("ODMyMjE3MzIyOTgyNDczNzk4.YHgkxw.f2TwACwDW2qxSOuzJtXrOwnuGYI")
